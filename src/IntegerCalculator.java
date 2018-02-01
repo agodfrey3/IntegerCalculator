1d572b6876e046d7422e2264362272525d62df07
Andrew Godfrey <agodfrey3@fordham.edu>
2018-01-31 20:59:04 -0500

    Fixed integer division issue.

--- src/IntegerCalculator.java
+++ src/IntegerCalculator.java
@@ -4,13 +4,12 @@
  * Program to implement a simple Integer Calculator with four operations.
  */
 
-import javax.swing.*;
 import java.awt.Button;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 public class IntegerCalculator  extends Frame {
     private static final long serialVersionUID = 1L;
@@ -19,37 +18,45 @@ public class IntegerCalculator  extends Frame {
     private TextField tf2;
     private Label l1;
     private Button bAdd, bSub, bMult, bDiv, bex;
+    private DecimalFormat df;
 
+    // Main method for handling all functionality.
     private IntegerCalculator() {
-        setSize(300, 370);
+        // Ensures that the output only contains one decimal place.
+        df = new DecimalFormat(".#");
+
+        ////////////////////////////////////////////////////////////
+        // Sets up the GUI.
+        ////////////////////////////////////////////////////////////
+        setSize(270, 200);
         this.setLocationRelativeTo(null);
 
         setTitle("Integer Calculator");
 
         tf1 = new TextField();
-        tf1.setBounds(15, 50, 85, 20);
+        tf1.setBounds(25, 50, 85, 20);
 
         tf2 = new TextField();
-        tf2.setBounds(15, 100, 85, 20);
+        tf2.setBounds(25, 100, 85, 20);
 
         bAdd = new Button("Add");
-        bAdd.setBounds(105,45, 60, 40);
+        bAdd.setBounds(115,45, 60, 40);
 
         bSub = new Button("Sub");
-        bSub.setBounds(170,45, 60, 40);
+        bSub.setBounds(180,45, 60, 40);
 
         bMult = new Button("Mult");
-        bMult.setBounds(105,95, 60, 40);
+        bMult.setBounds(115,95, 60, 40);
 
         bDiv = new Button("Div");
-        bDiv.setBounds(170,95, 60, 40);
+        bDiv.setBounds(180,95, 60, 40);
 
         bex = new Button("Exit");
-        bex.setBounds(170, 145,60,40);
+        bex.setBounds(180, 145,60,40);
 
         l1 = new Label();
         setLayout(null);
-        l1.setBounds(15, 150, 120, 20);
+        l1.setBounds(15, 150, 130, 20);
 
         add(bAdd);
         add(bSub);
@@ -60,36 +67,56 @@ public class IntegerCalculator  extends Frame {
         add(tf2);
         add(l1);
 
-        bAdd.addActionListener((ActionEvent e) -> {
-                int a = Integer.parseInt(tf1.getText());
-                int b = Integer.parseInt(tf2.getText());
-                int c = a + b;
+        ////////////////////////////////////////////////////////////
+        // Enables user actions and gracefully handles errors.
+        ///////////////////////////////////////////////////////////
 
-                l1.setText("Their sum is = " + String.valueOf(c));
+        bAdd.addActionListener((ActionEvent e) -> {
+            try {
+                double a = Double.parseDouble(tf1.getText());
+                double b = Double.parseDouble(tf2.getText());
+                double c = a + b;
+
+                l1.setText("Their sum is = " + df.format(c));
+            } catch (Exception NumberFormatException) {
+                l1.setText("Invalid input.");
+            }
         });
 
         bSub.addActionListener((ActionEvent e) -> {
-                int a = Integer.parseInt(tf1.getText());
-                int b = Integer.parseInt(tf2.getText());
-                int c = a - b;
+            try {
+                double a = Double.parseDouble(tf1.getText());
+                double b = Double.parseDouble(tf2.getText());
+                double c = a - b;
 
-                l1.setText("Their difference is = " + String.valueOf(c));
+                l1.setText("Their dif is = " + df.format(c));
+            } catch (Exception NumberFormatException) {
+                l1.setText("Invalid input.");
+            }
         });
 
         bMult.addActionListener((ActionEvent e) -> {
-            int a = Integer.parseInt(tf1.getText());
-            int b = Integer.parseInt(tf2.getText());
-            int c = a * b;
+            try {
+                double a = Double.parseDouble(tf1.getText());
+                double b = Double.parseDouble(tf2.getText());
+                double c = a * b;
 
-            l1.setText("Their product is = " + String.valueOf(c));
+                l1.setText("Their product is = " + df.format(c));
+            } catch (Exception NumberFormatException) {
+                l1.setText("Invalid input.");
+            }
         });
 
         bDiv.addActionListener((ActionEvent e) -> {
-            int a = Integer.parseInt(tf1.getText());
-            int b = Integer.parseInt(tf2.getText());
-            int c = a / b;
+            try {
+                double a = Double.parseDouble(tf1.getText());
+                double b = Double.parseDouble(tf2.getText());
+                double c = a / b;
 
-            l1.setText("The quotient is = " + String.valueOf(c));
+                l1.setText("The quotient is = " + df.format(c));
+            } catch (Exception NumberFormatException) {
+                l1.setText("Invalid input.");
+            }
         });
 
         bex.addActionListener((ActionEvent e) -> System.exit(0));
